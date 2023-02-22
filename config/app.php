5363f54e749bcc2e073b9ddf93a1018e52237af8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-02-23 00:47:19 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Laravel\Sanctum\SanctumServiceProvider::class,
 
         /*
          * Package Service Providers...
